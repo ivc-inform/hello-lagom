8cf515e2bd945e5deb4ff3405e6b88355e8d60d3
andrew <uandrew1965@gmail.com>
2018-02-02 12:52:43 +0300

    .

--- hello-lagom-api/src/main/scala/com/example/hello/api/HellolagomService.scala
+++ hello-lagom-api/src/main/scala/com/example/hello/api/HellolagomService.scala
@@ -6,8 +6,8 @@ import com.lightbend.lagom.scaladsl.api.broker.kafka.{KafkaProperties, Partition
 import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
 import play.api.libs.json.{Format, Json}
 
-object HellolagomService  {
-  val TOPIC_NAME = "greetings"
+object HellolagomService {
+    val TOPIC_NAME = "greetings"
 }
 
 /**
@@ -18,25 +18,25 @@ object HellolagomService  {
   */
 trait HellolagomService extends Service {
 
-  /**
-    * Example: curl http://localhost:9010/api/hello/Alice
-    */
-  def hello(id: String): ServiceCall[NotUsed, String]
+    /**
+      * Example: curl http://localhost:9010/api/hello/Alice
+      */
+    def hello(id: String): ServiceCall[NotUsed, String]
 
-  /**
-    * Example: curl -H "Content-Type: application/json" -X POST -d '{"message": "Hi"}' http://localhost:9010/api/hello/Alice
-    */
-  def useGreeting(id: String): ServiceCall[GreetingMessage, Done]
+    /**
+      * Example: curl -H "Content-Type: application/json" -X POST -d '{"message": "Hi"}' http://localhost:9010/api/hello/Alice
+      */
+    def useGreeting(id: String): ServiceCall[GreetingMessage, Done]
 
 
-  /**
-    * This gets published to Kafka.
-    */
-  def greetingsTopic(): Topic[GreetingMessageChanged]
+    /**
+      * This gets published to Kafka.
+      */
+    def greetingsTopic(): Topic[GreetingMessageChanged]
 
-  override final def descriptor = {
-    import Service._
-    // @formatter:off
+    override final def descriptor = {
+        import Service._
+        // @formatter:off
     named("hello-lagom")
       .withCalls(
         pathCall("/api/hello/:id", hello _),
@@ -49,14 +49,11 @@ trait HellolagomService extends Service {
           // go to the same partition (and hence are delivered in order with respect
           // to that user), we configure a partition key strategy that extracts the
           // name as the partition key.
-          .addProperty(
-            KafkaProperties.partitionKeyStrategy,
-            PartitionKeyStrategy[GreetingMessageChanged](_.name)
-          )
+          .addProperty(KafkaProperties.partitionKeyStrategy, PartitionKeyStrategy[GreetingMessageChanged](_.name))
       )
       .withAutoAcl(true)
     // @formatter:on
-  }
+    }
 }
 
 /**
@@ -65,14 +62,13 @@ trait HellolagomService extends Service {
 case class GreetingMessage(message: String)
 
 object GreetingMessage {
-  /**
-    * Format for converting greeting messages to and from JSON.
-    *
-    * This will be picked up by a Lagom implicit conversion from Play's JSON format to Lagom's message serializer.
-    */
-  implicit val format: Format[GreetingMessage] = Json.format[GreetingMessage]
+    /**
+      * Format for converting greeting messages to and from JSON.
+      *
+      * This will be picked up by a Lagom implicit conversion from Play's JSON format to Lagom's message serializer.
+      */
+    implicit val format: Format[GreetingMessage] = Json.format[GreetingMessage]
 }
-
 
 
 /**
@@ -82,10 +78,10 @@ object GreetingMessage {
 case class GreetingMessageChanged(name: String, message: String)
 
 object GreetingMessageChanged {
-  /**
-    * Format for converting greeting messages to and from JSON.
-    *
-    * This will be picked up by a Lagom implicit conversion from Play's JSON format to Lagom's message serializer.
-    */
-  implicit val format: Format[GreetingMessageChanged] = Json.format[GreetingMessageChanged]
+    /**
+      * Format for converting greeting messages to and from JSON.
+      *
+      * This will be picked up by a Lagom implicit conversion from Play's JSON format to Lagom's message serializer.
+      */
+    implicit val format: Format[GreetingMessageChanged] = Json.format[GreetingMessageChanged]
 }
